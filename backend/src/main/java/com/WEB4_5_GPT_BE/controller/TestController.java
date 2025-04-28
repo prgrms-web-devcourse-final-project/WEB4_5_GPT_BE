7eb66ecc43222f549d1806e466981c6ed890e1fb
SeongCheol Jung <kir5121@naver.com>
2025-04-28 15:43:20 +0900

    test: 컨벤션 적용

--- backend/src/main/java/com/WEB4_5_GPT_BE/controller/TestController.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/controller/TestController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-    @GetMapping
-    public String home() {
-        return "Welcome to Unihub!";
-    }
+  @GetMapping
+  public String home() {
+    return "Welcome to Unihub!";
+  }
 }
