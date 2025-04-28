e4822902a00494bfdad94c783e99db16d49f84dd
SeongCheol Jung <kir5121@naver.com>
2025-04-28 15:34:38 +0900

    test: github action 테스트용 controller 추가

---
+++ backend/src/main/java/com/WEB4_5_GPT_BE/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.WEB4_5_GPT_BE.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+    @GetMapping
+    public String home() {
+        return "Welcome to Unihub!";
+    }
+}
