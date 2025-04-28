96323dbe37f9350d305d7d2f960c19c062c600d9
jeonghyeon-ohk <ok6737@naver.com>
2025-04-29 04:20:22 +0900

    feat: 상위 루트 폴더에 infra 패키지를 생성하고 terraform을 통해 AWS 및 EC2 설정 관리
    - 중요 key 값은 secrets.tf를 통해 관리하며 양식은 secrets.tf.default를 통해 공유
    - 프리티어 제품 임시 적용함
    - EC2 생성 시 docker를 통해 npm, redis, mysql이 자동 실행되도록 명령어 구현

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "unihub"
+}
+
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
+
+variable "nickname" {
+  description = "nickname"
+  default     = "gptplz"
+}
