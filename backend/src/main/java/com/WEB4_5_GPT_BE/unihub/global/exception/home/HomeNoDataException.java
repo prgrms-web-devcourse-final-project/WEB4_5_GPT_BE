575668c88d50361394914428aa76e32ed2983c82
jeonghyeon-ohk <ok6737@naver.com>
2025-04-28 15:26:00 +0900

    chore: 테스트용 HomeController와 HomeNoDataException 제거

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/exception/home/HomeNoDataException.java
+++
@@ -1,9 +0,0 @@
-package com.WEB4_5_GPT_BE.unihub.global.exception.home;
-
-import com.WEB4_5_GPT_BE.unihub.global.exception.UnihubException;
-
-public class HomeNoDataException extends UnihubException {
-    public HomeNoDataException(String code, String message) {
-        super(code, message);
-    }
-}
