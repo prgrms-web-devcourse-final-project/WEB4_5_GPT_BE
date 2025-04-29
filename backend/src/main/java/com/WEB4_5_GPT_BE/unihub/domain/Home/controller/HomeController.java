93cecb2e4f8ac2ff55fa20def65e1659b150abdb
jeonghyeon-ohk <ok6737@naver.com>
2025-04-30 01:57:35 +0900

    feat: spotlessApply 적용

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/Home/controller/HomeController.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/Home/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-     @GetMapping
-     public String home() {
-         return "Welcome to the Unihub API!";
-     }
+  @GetMapping
+  public String home() {
+    return "Welcome to the Unihub API!";
+  }
 }
