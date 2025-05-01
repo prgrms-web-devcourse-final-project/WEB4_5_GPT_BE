4e51b13c29e2fbbe291a0210b474c1902467b3fa
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2025-05-01 12:04:23 +0900

    feat: 강의 스케줄 DTO 추가

---
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/course/dto/CourseScheduleDto.java
@@ -0,0 +1,36 @@
+package com.WEB4_5_GPT_BE.unihub.domain.course.dto;
+
+import com.WEB4_5_GPT_BE.unihub.domain.common.enums.DayOfWeek;
+import com.WEB4_5_GPT_BE.unihub.domain.course.entity.Course;
+import com.WEB4_5_GPT_BE.unihub.domain.course.entity.CourseSchedule;
+import jakarta.validation.constraints.NotEmpty;
+
+import java.time.LocalTime;
+
+public record CourseScheduleDto(
+        @NotEmpty String day,
+        @NotEmpty String startTime,
+        @NotEmpty String endTime
+) {
+    public static CourseScheduleDto from(CourseSchedule courseSchedule) {
+        return new CourseScheduleDto(
+                courseSchedule.getDay().toString(),
+                courseSchedule.getStartTime().toString(),
+                courseSchedule.getEndTime().toString()
+        );
+    }
+
+    public CourseSchedule toEntity(Course course, Long universityId) {
+        return new CourseSchedule(
+                null,
+                course,
+                universityId,
+                course.getLocation(),
+                course.getProfessor() != null ? course.getProfessor().getEmployeeId() : null,
+                DayOfWeek.valueOf(day),
+                // TODO
+                LocalTime.parse(startTime),
+                LocalTime.parse(endTime)
+        );
+    }
+}
