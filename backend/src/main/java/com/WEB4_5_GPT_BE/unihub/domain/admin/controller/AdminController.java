7626b6be22ada6f1cd9ef01ab6821efff691b56a
theGreatKang98 <srouc159@gmail.com>
2025-05-01 14:11:58 +0900

    [fix] admin 인가 추가

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/admin/controller/AdminController.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/admin/controller/AdminController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
+//@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
   private final AdminService adminService;
