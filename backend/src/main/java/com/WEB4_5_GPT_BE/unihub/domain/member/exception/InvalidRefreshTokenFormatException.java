09eef19a9c7acb2fdd2e0d9e58b87d053e750344
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-01 09:55:07 +0900

    refactor: API 명세서에 맞게 메세지 수정

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/exception/InvalidRefreshTokenFormatException.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/exception/InvalidRefreshTokenFormatException.java
@@ -4,6 +4,6 @@ import com.WEB4_5_GPT_BE.unihub.global.exception.AuthException;
 
 public class InvalidRefreshTokenFormatException extends AuthException {
     public InvalidRefreshTokenFormatException() {
-        super("400", "잘못된 RefreshToken 형식입니다.");
+        super("400", "잘못된 refreshToken 형식입니다.");
     }
 }
