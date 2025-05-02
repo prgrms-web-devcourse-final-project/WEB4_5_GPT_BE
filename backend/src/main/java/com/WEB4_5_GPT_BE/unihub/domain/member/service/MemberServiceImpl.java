c456daf07c1d1a20218d6848d98ebffe2cb871ca
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-02 09:45:42 +0900

    refactor: ProfessorSignupRequest -> ProfessorSignUpRequest

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/service/MemberServiceImpl.java
@@ -2,10 +2,7 @@ package com.WEB4_5_GPT_BE.unihub.domain.member.service;
 
 import com.WEB4_5_GPT_BE.unihub.domain.common.enums.ApprovalStatus;
 import com.WEB4_5_GPT_BE.unihub.domain.common.enums.Role;
-import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.EmailCodeVerificationRequest;
-import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.PasswordResetConfirmationRequest;
-import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.ProfessorSignupRequest;
-import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.StudentSignUpRequest;
+import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.*;
 import com.WEB4_5_GPT_BE.unihub.domain.member.dto.request.mypage.*;
 import com.WEB4_5_GPT_BE.unihub.domain.member.dto.response.mypage.MyPageProfessorResponse;
 import com.WEB4_5_GPT_BE.unihub.domain.member.dto.response.mypage.MyPageStudentResponse;
@@ -95,7 +92,7 @@ public class MemberServiceImpl implements MemberService {
   }
 
   @Override
-  public void signUpProfessor(ProfessorSignupRequest request) {
+  public void signUpProfessor(ProfessorSignUpRequest request) {
     University university = universityService.getUniversity(request.universityId());
     Major major = majorService.getMajor(request.universityId(), request.majorId());
     validateEmailVerification(request.email());
@@ -123,7 +120,7 @@ public class MemberServiceImpl implements MemberService {
     memberRepository.save(member);
   }
 
-    private void validateProfessorSignUp(ProfessorSignupRequest request) {
+    private void validateProfessorSignUp(ProfessorSignUpRequest request) {
     if (memberRepository.existsByEmail(request.email())) {
       throw new UnihubException("409", "이메일 또는 사번이 이미 등록되어 있습니다.");
     }
