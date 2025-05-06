75bc07bc7dcf645d17743bd2d211bb997204e84f
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-06 15:06:54 +0900

    refactor: 사용하지 않는 ROLE에 NotNull제거 및 swagger를 위한 설명 추가

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/dto/request/ProfessorSignUpRequest.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/dto/request/ProfessorSignUpRequest.java
@@ -1,6 +1,7 @@
 package com.WEB4_5_GPT_BE.unihub.domain.member.dto.request;
 
 import com.WEB4_5_GPT_BE.unihub.domain.common.enums.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -12,4 +13,5 @@ public record ProfessorSignUpRequest(
     @NotBlank(message = "필수 입력 값이 누락되었습니다") String employeeId,
     @NotNull(message = "필수 입력 값이 누락되었습니다") Long universityId,
     @NotNull(message = "필수 입력 값이 누락되었습니다") Long majorId,
-    @NotNull(message = "필수 입력 값이 누락되었습니다") Role role) {}
+    @Schema(description = "항상 PROFESSOR입니다. 서버에서 자동 설정되며, 요청 시 생략해도 무방합니다.", allowableValues = {"PROFESSOR"})
+    Role role) {}
