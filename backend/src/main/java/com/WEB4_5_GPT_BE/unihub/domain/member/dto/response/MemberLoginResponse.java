e35ec6f4b29bf29bd6c443146b89bc077cc436e3
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-10 17:21:46 +0900

    develop -> main으로 배포 (#108)
    * docs: develop 초기 데이터 최신화를 위해 ddl-auto를 create로 수정하여 재배포
    
    * docs: 테스트 서버 초기 데이터 최신화 후 ddl-auto를 다시 update로 변경
    
    * 강을찬/fix 버그와 api명세서와 다른것 수정 (#90)
    
    * [fix] security user 에서 authentication principal 빠진것 추가
    
    * [fix] api endpoint enrollment 에서 enrollments 로 수정
    
    * [fix] AdminController 에서 전공, 대학교 관련 생성,수정,삭제 api 빠진것들 다시 생성
    
    * [fix] test code 에서도 enrollment 대신 enrollments 쓰도록 수정
    
    * [feat] 관리자 api PreAuthorize 추가해 관리자만 접근 가능하도록 수정 (#95)
    
    * studentprofilereponse,professorprofilereponse에 university_id 추가 (#100)
    
    * 로그인시 refreshToken을 Reponse에서 제거, refreshToken의 저장기간을 1일로 변경 (#104)
    
    * refactor: refreshToken을 cookie로만 응답하게 하고, response에서 refreshToken은 삭제
    
    * refactor: refreshToken을 cookie로만 전달하도록 변경, refreshToken의 저장 기간을 1일 로 변경
    
    ---------
    
    Co-authored-by: jeonghyeon-ohk <ok6737@naver.com>
    Co-authored-by: TheGreatKang <77500386+TheGreatKang@users.noreply.github.com>
    Co-authored-by: sojunsik <113578269+sojunsik@users.noreply.github.com>

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/dto/response/MemberLoginResponse.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/member/dto/response/MemberLoginResponse.java
@@ -2,4 +2,4 @@ package com.WEB4_5_GPT_BE.unihub.domain.member.dto.response;
 
 import com.WEB4_5_GPT_BE.unihub.domain.common.enums.Role;
 
-public record MemberLoginResponse(String accessToken, String refreshToken, Long id, String email, Role role) {}
+public record MemberLoginResponse(String accessToken, Long id, String email, Role role) {}
