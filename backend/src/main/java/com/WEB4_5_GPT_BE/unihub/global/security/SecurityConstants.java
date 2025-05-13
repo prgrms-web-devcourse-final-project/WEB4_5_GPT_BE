d08f698e4a0f12d16a1c3990907b057ad9218338
jeonghyeon-ohk <ok6737@naver.com>
2025-05-14 01:42:08 +0900

    refactor : s3 테스트하면서 예외에 추가했던 api 주소 제거 및 중복된 h2-console 주소 제거

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/security/SecurityConstants.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/security/SecurityConstants.java
@@ -20,7 +20,6 @@ public class SecurityConstants {
           "/swagger-ui/**",
           "/v3/api-docs/**",
           "/h2-console/**",
-              "/actuator/**",
-              "/api/file/**"
+              "/actuator/**"
       );
 }
