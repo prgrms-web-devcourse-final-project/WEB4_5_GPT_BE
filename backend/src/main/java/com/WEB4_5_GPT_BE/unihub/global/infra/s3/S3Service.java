588c969245dfe41349470860459e1603aed5228f
jeonghyeon-ohk <ok6737@naver.com>
2025-05-15 17:18:38 +0900

    feat : 파일 명이 없을경우 unknown으로 저장되도록 로직 추가, activeProfile에 따라 다르게 저장되도록 추가구현

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/infra/s3/S3Service.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/infra/s3/S3Service.java
@@ -14,6 +14,7 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Optional;
 
 /**
  * AWS S3에 관련된 비즈니스 로직을 처리하는 서비스입니다.
@@ -27,9 +28,13 @@ public class S3Service {
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucketName;
 
+    // 활성 프로파일 (예: "dev", "prod")
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
     /**
      * MultipartFile을 S3에 업로드하고, 업로드된 파일의 URL을 반환합니다.
-     * <p>
+     *
      * 업로드된 파일은 S3에 저장되며, URL을 통해 접근할 수 있습니다.
      * S3에 퍼블릭 읽기 권한을 부여하고 고정적인 URL을 부여받기 위해 버킷 정책(Bucket Policy)이 설정되어 있어야 합니다.
      *
@@ -40,7 +45,7 @@ public class S3Service {
     public String upload(MultipartFile file) throws IOException {
 
         // 1) 한글 인코딩 처리 및 공백을 "_"(언더바)로 대체
-        String filename = file.getOriginalFilename();                                  // "자료구조 강의계획서.png"
+        String filename = Optional.ofNullable(file.getOriginalFilename()).orElse("unknown");                                  // "자료구조 강의계획서.png"
         String replacedSpaces = filename.replaceAll("\\s+", "_"); // "자료구조_강의계획서.png"
         String encoded = URLEncoder.encode(replacedSpaces, StandardCharsets.UTF_8);
 
@@ -49,14 +54,12 @@ public class S3Service {
         file.transferTo(filePath.toFile());
 
         // 3) S3에 업로드할 키 생성
-        String key = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String key = "%s/%s-%s".formatted(activeProfile, timestamp, encoded); // ex) dev/1684092000000-자료구조_강의계획서.png
 
         // 4) S3에 파일 업로드 (KEY, FILE)
         s3Client.putObject(
-                PutObjectRequest.builder()
-                        .bucket(bucketName)
-                        .key(key)
-                        .build(),
+                PutObjectRequest.builder().bucket(bucketName).key(key).build(),
                 filePath
         );
 
