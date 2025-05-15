89bc144acd228eb575c81d8eb6a0ee286cd55238
SeongCheol Jung <60413291+jsc5023@users.noreply.github.com>
2025-05-15 17:29:40 +0900

    refactor : error메세지 -> 비공개 시간표 본인만 열람가능 으로 변경

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/timetable/exception/share/TimetablePrivateException.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/timetable/exception/share/TimetablePrivateException.java
@@ -4,6 +4,6 @@ import com.WEB4_5_GPT_BE.unihub.domain.timetable.exception.TimetableException;
 
 public class TimetablePrivateException extends TimetableException {
     public TimetablePrivateException() {
-        super("403", "해당 시간표는 비공개입니다.");
+        super("403", "비공개 시간표는 본인만 열람할 수 있습니다.");
     }
 }
