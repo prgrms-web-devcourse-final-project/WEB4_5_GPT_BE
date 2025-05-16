1fa0f6a3b8a7f84a10a636024cfdcf17accab508
theGreatKang98 <srouc159@gmail.com>
2025-05-16 17:41:56 +0900

    [feat] 시간표 item 관련 api 완성

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/timetable/service/TimetableItemService.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/domain/timetable/service/TimetableItemService.java
@@ -1,21 +1,22 @@
 package com.WEB4_5_GPT_BE.unihub.domain.timetable.service;
 
 import com.WEB4_5_GPT_BE.unihub.domain.member.entity.Member;
+import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.request.item.TimetableBulkRegisterRequest;
 import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.request.item.TimetableCourseAddRequest;
 import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.request.item.TimetableItemNormalCreateRequest;
+import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.request.item.TimetableItemUpdateRequest;
 import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.response.item.TimetableItemDetailResponse;
-import com.WEB4_5_GPT_BE.unihub.domain.timetable.dto.response.item.TimetableItemUpdateRequest;
 
 public interface TimetableItemService {
 
     // 시간표에 직접 등록
-    void addCustomItem(Member member, TimetableItemNormalCreateRequest request);
+    void createNormalItem(Member member, TimetableItemNormalCreateRequest request);
 
     // 시간표에 강의 등록
     void addCourseItem(Member member, TimetableCourseAddRequest request);
 
     // 수강 중인 강의 전체 반영
-    void bulkRegisterFromEnrollment(Member member);
+    void bulkRegisterFromEnrollment(Member member, TimetableBulkRegisterRequest request);
 
     // 시간표 항목 단건 조회
     TimetableItemDetailResponse getItemDetail(Member member, Long timetableItemId);
