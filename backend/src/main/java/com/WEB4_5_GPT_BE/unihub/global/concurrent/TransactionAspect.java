191937dacffaa7b51d9040e116b0e98c6cd4b4b1
jeonghyeon-ohk <ok6737@naver.com>
2025-05-30 16:41:44 +0900

    refactor: test에서도 동작하도록 redisson 설정 수정

--- backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/concurrent/TransactionAspect.java
+++ backend/src/main/java/com/WEB4_5_GPT_BE/unihub/global/concurrent/TransactionAspect.java
@@ -1,21 +1,19 @@
 package com.WEB4_5_GPT_BE.unihub.global.concurrent;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
  * [분산 락 적용 시 내 비즈니스 로직을 독립 트랜잭션으로 실행하기 위한 Aspect]
- * <p>
+ *
  * 분산 락 처리와는 별도로 새로운 트랜잭션을 시작하여 비즈니스 로직을 처리하도록 합니다.
  * 락 → (새 트랜잭션) → 비즈니스 로직 실행 → (새 트랜잭션 커밋) → 락 해제
  * 순서를 확실히 분리함으로써 다른 스레드는 커밋이 완료된 데이터만을 사용할 수 있게되며
  * 이를 통해 데이터 정합성을 보장할 수 있습니다.
  */
 @Component
-@Profile("!test")
 public class TransactionAspect {
     /**
      * [propagation = REQUIRES_NEW]
